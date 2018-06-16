43f4740e8d89958c0a0330df7515a667c53e7217
Lucas Ferreira <lucas.santos61@fatec.sp.gov.br>
2018-06-16 17:20:47 -0300

    Initial commit

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    "development" => [
+        'driver' =>'mysql',
+        'host' => 'localhost',
+        'database' => 'financas',
+        'username' => 'root',
+        'password' => '123',
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci'
+    ]
+];
