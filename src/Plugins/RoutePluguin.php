d6f6faa0ec3035590f80e961a8551294b9e93220
Lucas Ferreira <lucas.santos61@fatec.sp.gov.br>
2018-06-19 22:26:47 -0300

    Finish routes service

--- src/Plugins/RoutePluguin.php
+++ src/Plugins/RoutePluguin.php
@@ -1,9 +1,12 @@
 <?php
-
+declare(strict_types=1);
 namespace Financas\Plugins;
 
 use Financas\ServiceContainerInterface;
 use Aura\Router\RouterContainer;
+use Psr\Http\Message\RequestInterface;
+use Zend\Diactoros\ServerRequestFactory;
+use Interop\Container\ContainerInterface;
 
 
 
@@ -14,12 +17,24 @@ class RoutePluggin implements PluginInterface
         $routeContainer = new RouterContainer();
         //Registra Rotas da palicação
         $map = $routeContainer->getMap();
-        //Tem funcção de indentificar a rota que está sendo acessada
+        //Tem função de indentificar a rota que está sendo acessada
         $matcher = $routeContainer->getMatcher();
-        //Tem funçãoi de gerar links  com base nas rotas registradas
+        //Tem função de gerar links  com base nas rotas registradas
         $generator  = $routeContainer->getGenerator();
+        $request  = $this->getRequest();
         $container->add('routing',$map);
         $container->add('routing.matcher',$matcher);
         $container->add('routing.generator',$generator);
+        $container->add(RequestInterface::class,$request);
+        $container->addLazy('route',function(ContainerInterface $container){
+           $matcher =  $container->get('router.matcher');
+           $request = $container->get(RequestInterface::class);
+          return $matcher->match($request);
+        });
+    }
+
+    protected function getRequest(): RequestInterface
+    {
+        return ServerRequestFactory::fromGlobals($_SERVER,$_GET,$_POST,$_COOKIE,$_FILES);
     }
 }
