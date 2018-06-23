f835a3dee722b7c167c1eb34091d3268a653ce7c
Lucas Ferreira <lucas.santos61@fatec.sp.gov.br>
2018-06-23 17:01:38 -0300

    Add list view and route view

--- public/index.php
+++ public/index.php
@@ -9,6 +9,7 @@ use Zend\Diactoros\Response;
 use Psr\Http\Message\RequestInterface;
 use Financas\Plugins\DbPlugin;
 use Financas\Models\CategoryCost;
+use Zend\Diactoros\Response\RedirectResponse;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
@@ -18,11 +19,24 @@ $app = new Application($serviceContaner);
 $app->plugin(new RoutePlugin());
 $app->plugin(new ViewPlugin());
 $app->plugin(new DbPlugin());
-$app->get('/category-costs', function() use($app){
+$app
+    ->get('/category-costs', function() use($app){
+        $view = $app->service('view.renderer');
+        $myModel = new CategoryCost();
+        $categories = $myModel->all();
+        return $view->render('category-costs/list.html.twig',['categories'=>$categories]);
+    })
+
+    ->get('/category-costs/new', function() use($app){
     $view = $app->service('view.renderer');
     $myModel = new CategoryCost();
     $categories = $myModel->all();
-    return $view->render('category-costs/list.html.twig',['categories'=>$categories]);
-});
-
+    return $view->render('category-costs/create.html.twig');
+    })
+    ->post('/category-costs/store', function(ServerRequestInterface $request){
+        $data = $request->getParsedBody();
+        
+        CategoryCost::create($data);
+        return new RedirectResponse('/category-costs');
+    });
 $app->start();
