555525e06a6ce0964562e671877a7b5b9e44a324
Lucas Ferreira <lucas.santos61@fatec.sp.gov.br>
2018-06-25 22:43:04 -0300

    Hotfix

--- public/index.php
+++ public/index.php
@@ -4,11 +4,9 @@ use Financas\ServiceContainer;
 use Financas\Application;
 use Financas\Plugins\RoutePlugin;
 use Financas\Plugins\ViewPlugin;
-use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response;
 use Psr\Http\Message\RequestInterface;
 use Financas\Plugins\DbPlugin;
-use Financas\Models\CategoryCost;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
